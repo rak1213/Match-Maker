b00dc1c9ef374f5aa8a8330004b5f8fbcad65246
Rakshit Gupta <rakshitgupta@Rakshits-MacBook-Air-2.local>
2023-11-07 19:07:51 -0400

    Different visulaizations at different random seeds

--- config.py
+++ config.py
@@ -3,3 +3,9 @@ BASE_RESULTS = 'Results'
 TRANSFORMER_MINILM_L6_V2 = 'sentence-transformers/all-MiniLM-L6-v2'
 TRANSFORMER_MPNET_BASE_V2 = 'sentence-transformers/all-mpnet-base-v2'
 
+SAVE_EMBEDDINGS = 0
+
+RANDOM_STATE = 42
+#RANDOM_STATE = 0
+#RANDOM_STATE = 23
+
---
+++ umap_enhancement.py
@@ -0,0 +1,9 @@
+import umap
+import numpy as np
+import optuna
+from scipy.spatial.distance import cosine
+from scipy.stats import spearmanr
+
+
+
+def objective
