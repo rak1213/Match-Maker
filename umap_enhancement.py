b69c01c74f7ab668547105f8b61007c94f94649a
Rakshit Gupta <rakshitgupta@Rakshits-MacBook-Air-2.local>
2023-11-07 19:35:42 -0400

    Code updated for umap enhancement

--- umap_enhancement.py
+++ umap_enhancement.py
@@ -1,9 +1,48 @@
 import umap
 import numpy as np
+import json
 import optuna
+import config as CONFIG
 from scipy.spatial.distance import cosine
 from scipy.stats import spearmanr
 
+def load_embeddings(file_path):
+    """Load embeddings from a JSON file."""
+    with open(file_path, 'r') as file:
+        embeddings = json.load(file)
+    return embeddings
+
+def umap_objective(trial):
+    # Define the hyperparameters to tune
+    n_neighbors = trial.suggest_int('n_neighbors', 5, 50)
+    min_dist = trial.suggest_float('min_dist', 0.0, 0.9)
+
+    reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=CONFIG.RANDOM_STATE)
+
+    path = os.path.join(CONFIG.BASE_RESULTS,'person_embeddings_minilm.json')
+    embeddings = load_embeddings()
+    embedding_2d = reducer.fit_transform(embeddings)
+
+    # Compute Spearman rank correlations
+    correlations = []
+    for i in range(len(embeddings)):
+        # Compute cosine similarities for high-dimensional embeddings
+        cos_similarities = np.array([1 - cosine(embeddings[i], emb) for emb in embeddings])
+
+        # Compute Euclidean distances in the 2D space
+        euclidean_dists = np.linalg.norm(embedding_2d - embedding_2d[i], axis=1)
+
+        # Compute rankings
+        cos_ranks = np.argsort(cos_similarities)
+        euclidean_ranks = np.argsort(euclidean_dists)
+
+        # Compute and store Spearman rank correlation
+        correlation, _ = spearmanr(cos_ranks, euclidean_ranks)
+        correlations.append(correlation)
+
+    # Return the average correlation
+    return np.mean(correlations)
 
 
-def objective
+if __name__ == '__main__':  
+    pass
